2d3522681ccb6bf67013a29c9fdeeef75509c7e8
Shawn Dove <shawn.dove@hotmail.com>
2026-03-24 00:22:09 +0100

    switch to r native pipe

--- R/get_ramsar_occ_density.R
+++ R/get_ramsar_occ_density.R
@@ -1,7 +1,7 @@
 get_ramsar_occ_density <- function(total_occ_map) {
   x <- total_occ_map
-  area <- x$original_bbox %>%
-    sf::st_area() %>%
+  area <- x$original_bbox |>
+    sf::st_area() |>
     units::set_units("km^2")
   occ <- sum(x$data$diversity_val, na.rm = TRUE)
   density <- occ / area
